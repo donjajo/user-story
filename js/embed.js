93b5ded5d74bfc3a6cf5cd2cdd1e26695086916f
James Etu <me@donjajo.com>
2025-06-16 02:12:28 +0000

    JS embed completed

--- js/embed.js
+++ js/embed.js
@@ -1,30 +1,101 @@
 'use strict';
 
-const sanizeHref = ( href ) => {
-	const url = new URL( href );
+import { sha1 } from 'js-sha1';
+import apiFetch from '@wordpress/api-fetch';
 
-	// console.log( url );
+const sanizeHref = ( href ) => {
+	try {
+		const url = new URL( href );
+
+		return url.href;
+	} catch ( TypeError ) {
+		return null;
+	}
 };
-const userStoryObserverCallback = ( entries, observer ) => {
-	const visibleLinks = []
+
+const userStoryMkLink = ( target ) => {
+	return {
+		url: sanizeHref( target ),
+		name: target.textContent,
+		x: target.getBoundingClientRect().x,
+		y: target.getBoundingClientRect().y,
+	};
+};
+
+const userStoryGetHash = () => {
+	return window.sessionStorage.getItem( 'userStoryHash' );
+};
+
+const userStorySetHash = ( hash ) => {
+	window.sessionStorage.setItem( 'userStoryHash', hash );
+};
+
+const userStoryUpdateDevice = ( id ) => {
+	window.localStorage.setItem( 'userStoryDeviceId', id );
+};
+
+const userStoryGetDevice = () => {
+	return window.localStorage.getItem( 'userStoryDeviceId' );
+};
+
+const userStoryUpdate = async ( links ) => {
+	return apiFetch( {
+		path: '/user-story/links',
+		method: 'POST',
+		parse: false,
+		headers: {
+			'X-Device': userStoryGetDevice() || '',
+		},
+		data: {
+			links,
+			height: window.screen.height,
+			width: window.screen.width,
+		},
+	} )
+		.then( ( response ) => {
+			userStoryUpdateDevice( response.headers.get( 'X-Device' ) );
+			return true;
+		} )
+		.catch( () => {
+			return false;
+		} );
+};
+const userStoryObserverCallback = ( entries ) => {
+	const visibleLinks = [];
+	let hashValue = '';
+
 	entries.forEach( ( entry ) => {
 		if ( entry.isIntersecting ) {
-			visibleLinks.push( sanizeHref( entry.target ) );
-			console.log( entry.isIntersecting, entry.target );
+			if ( sanizeHref( entry.target ) !== null ) {
+				const link = userStoryMkLink( entry.target );
+
+				hashValue += `${ link.url }${ link.name }${ link.x }${ link.y }`;
+				visibleLinks.push( link );
+			}
 		}
 
 		// We don't need to observe anymore after page load
 		userStoryObserver.unobserve( entry.target );
 	} );
+
+	const hash = sha1( hashValue );
+	if ( hash !== userStoryGetHash() ) {
+		userStoryUpdate( visibleLinks ).then( ( re ) => {
+			if ( re ) {
+				userStorySetHash( hash );
+			}
+		} );
+	}
 };
 
-const userStoryObserver = new IntersectionObserver( userStoryObserverCallback, {
-	rootMargin: '0px',
-	threshold: 0.5,
-} );
+const userStoryObserver = new window.IntersectionObserver(
+	userStoryObserverCallback,
+	{
+		rootMargin: '0px',
+		threshold: 0.5,
+	}
+);
 const userStoryInit = () => {
-	sessionStorage.setItem( 'visibleLinks', [] );
-
 	document.querySelectorAll( 'a[href]' ).forEach( ( link ) => {
 		userStoryObserver.observe( link );
 	} );
