fff69eecab2834509361e5cfcb2a5ff9af33ecaa
James Etu <me@donjajo.com>
2025-06-17 00:37:23 +0000

    Fix phpcs

--- functions.php
+++ functions.php
@@ -153,18 +153,17 @@ function user_story_get_site_host() {
  *
  * @param string|null $data Optional 16 bytes of binary data used for UUID generation. If not provided, random data will be used.
  * @return string A 36-character string representing the UUID v4.
- * @throws \Random\RandomException
  */
-function user_story_uuid4($data = null) {
+function user_story_uuid4( $data = null ) {
 	// Generate 16 bytes (128 bits) of random data or use the data passed into the function.
-	$data = $data ?? random_bytes(16);
-	assert(strlen($data) == 16);
+	$data = $data ?? random_bytes( 16 );
+	assert( strlen( $data ) === 16 );
 
-	// Set version to 0100
-	$data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-	// Set bits 6-7 to 10
-	$data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+	// Set version to 0100.
+	$data[6] = chr( ord( $data[6] ) & 0x0f | 0x40 );
+	// Set bits 6-7 to 10.
+	$data[8] = chr( ord( $data[8] ) & 0x3f | 0x80 );
 
 	// Output the 36 character UUID.
-	return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+	return vsprintf( '%s%s-%s-%s-%s-%s%s%s', str_split( bin2hex( $data ), 4 ) );
 }
